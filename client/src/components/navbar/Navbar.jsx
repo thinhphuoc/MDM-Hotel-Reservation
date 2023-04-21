12a53a054d7d99c7e10eae09bd13a6ef0bccefec
sotreduch2602 <86106907+sotreduch2602@users.noreply.github.com>
2023-04-21 21:04:55 +0700

    Update login logout

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,28 +2,42 @@ import "./navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
-import { SearchContext } from "../../context/SearchContext";
-import { useState } from "react";
-  
+// import { SearchContext } from "../../context/SearchContext";
+// import { useState } from "react";
+import {useNavigate} from 'react-router-dom';
+
 
 
 const Navbar = () => {
-
-  const {user} = useContext(AuthContext);
-
-
+  const navigate = useNavigate();
+  const {user, dispatch} = useContext(AuthContext);
   console.log("Navbar",user);
+
+  const handleMoveToLogin = () =>  {
+    navigate('/login')
+  }
+
+  console.log(user)
+  const handleLogout = () =>  {
+    dispatch({type:"LOGOUT"});
+    navigate('/')
+  }
+
+
 
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to="/" style={{color:"inherit", textDecoration:"none"}}>
-        <span className="logo">Hotel Booking </span>
+        <span className="logo">Hotel Booking</span>
         </Link>   {/* if there is user show his username otherwise show this div.  */}
-        {user ? user.username : (
+        {user ? <div>
+          <span>{user.username}</span>
+          <button className="navButton" onClick={() => handleLogout()} >Logout</button>
+        </div> : (
           <div className="navItems">
             <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
+            <button className="navButton" onClick={() => handleMoveToLogin()} >Login</button>
           </div>
         )}
       </div>
