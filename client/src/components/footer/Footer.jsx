09a421c62ea60f23c7f5d0d693260c5ef62f46ad
thinhphuoc <63508962+thinhphuoc@users.noreply.github.com>
2023-04-22 22:48:19 +0700

    update footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
           <li className="fListItem">Travel Agents </li>
         </ul>
         <ul className="fList">
-          <li className="fListItem">Curtomer Service</li>
+          <li className="fListItem">Customer Service</li>
           <li className="fListItem">Partner Help</li>
           <li className="fListItem">Careers</li>
           <li className="fListItem">Sustainability</li>
@@ -44,7 +44,6 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2022 Lamabooking.</div>
     </div>
   );
 };
