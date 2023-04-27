14114be5dd02f64ee8b26366efaabff9d95bf4a9
nhut19112001 <nhutsadow@gmail.com>
2023-04-27 21:07:51 +0700

    Revert "Merge branch 'main' of https://github.com/thinhphuoc/MDM-Hotel-Reservation"
    This reverts commit 8d7ba68e9e865da6ff1dc6268da5d6b7033b9d69, reversing
    changes made to f9fd87130c48117f1200dd869f6f439686e9da09.

--- client/src/pages/booking/booking.jsx
+++ client/src/pages/booking/booking.jsx
@@ -2,14 +2,14 @@ import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./booking.css";
-import Table from "../../components/table/Table"
+
 const booking = () => {
     return (
       <div>
         <Navbar />
         <Header/>
         <div className="homeContainer">
-          <Table/>
+
         </div>
         <div className="homeContainer2">
           <Footer/>
