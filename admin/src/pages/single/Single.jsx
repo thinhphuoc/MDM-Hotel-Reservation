96187dd95a8fcd2a023a124f7a52e8b05bc8ef97
khoivankarik <khoivankarik@gmail.com>
2023-04-30 21:58:04 +0700

    update view user Admin

--- admin/src/pages/single/Single.jsx
+++ admin/src/pages/single/Single.jsx
@@ -3,10 +3,31 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/table/Table";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-const userId = window.location.pathname.split("/").pop();
+
 
 const Single = () => {
+
+  const userId = window.location.pathname.split("/").pop();
+  
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(`/users/${userId}`);
+        setUser(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchUser();
+  }, [userId]);
+
+  if (!user) return <div>Loading...</div>;
+
   return (
     <div className="single">
       <Sidebar />
@@ -18,29 +39,29 @@ const Single = () => {
             <h1 className="title">Information</h1>
             <div className="item">
               <img
-                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                src={user.img}
                 alt=""
                 className="itemImg"
               />
               <div className="details">
-                <h1 className="itemTitle">Jane Doe</h1>
+                <h1 className="itemTitle">{user.username}</h1>
                 <div className="detailItem">
                   <span className="itemKey">Email:</span>
-                  <span className="itemValue">janedoe@gmail.com</span>
+                  <span className="itemValue">{user.email}</span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Phone:</span>
-                  <span className="itemValue">+1 2345 67 89</span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Address:</span>
-                  <span className="itemValue">
-                    Elton St. 234 Garden Yd. NewYork
-                  </span>
+                  <span className="itemValue">{user.phone}</span>
                 </div>
                 <div className="detailItem">
                   <span className="itemKey">Country:</span>
-                  <span className="itemValue">USA</span>
+                  <span className="itemValue">
+                    {user.country}
+                  </span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">City:</span>
+                  <span className="itemValue">{user.city}</span>
                 </div>
               </div>
             </div>
