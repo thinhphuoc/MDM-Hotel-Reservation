e04e6382db89350f47fa2422ab39b9104f95e913
sotreduch2602 <86106907+sotreduch2602@users.noreply.github.com>
2023-05-01 21:05:22 +0700

    Update UserInfo

--- admin/src/pages/single/Single.jsx
+++ admin/src/pages/single/Single.jsx
@@ -70,10 +70,10 @@ const Single = () => {
             <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
           </div>
         </div>
-        <div className="bottom">
+        {/* <div className="bottom">
         <h1 className="title">Last Transactions</h1>
           <List/>
-        </div>
+        </div> */}
       </div>
     </div>
   );
