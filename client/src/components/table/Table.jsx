7f4133c3fab53719b0743903f726d3635bfe93db
sotreduch2602 <86106907+sotreduch2602@users.noreply.github.com>
2023-05-11 12:24:42 +0700

    UpdateUserBooking

--- client/src/components/table/Table.jsx
+++ client/src/components/table/Table.jsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,53 +7,61 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import "./Table.css"
-function createData(name, roomnumber, price, checkin, checkout, status) {
-    return { name, roomnumber, price, checkin, checkout, status };
-}
+import "./Table.css";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
+const BasicTable = () => {
+    const { user } = useContext(AuthContext);
+  const [rows, setRows] = useState([]);
+  const userId = user ? user._id : null; 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/bookings/${userId}`); // Replace '/api/users' with your backend API endpoint
+        setRows(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-const rows = [
-  createData('La Vela Saigon Hotel', 159, 1000, "11/1/2023", "11/1/2023", "success"),
-  createData('La Vela Saigon Hotel', 159, 1000, "11/1/2023", "11/1/2023", "success"),
-  createData('La Vela Saigon Hotel', 159, 1000, "11/1/2023", "11/1/2023", "success"),
-  createData('La Vela Saigon Hotel', 159, 1000, "11/1/2023", "11/1/2023", "success"),
-  createData('La Vela Saigon Hotel', 159, 1000, "11/1/2023", "11/1/2023", "success"),
-];
+    fetchData();
+  }, []);
 
-export default function BasicTable() {
   return (
     <div className='BookingTable'>
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    <TableCell sx={{ fontWeight: 'bold' }}              >Hotel Name</TableCell>
-                    <TableCell sx={{ fontWeight: 'bold' }} align="center">Room Number</TableCell>
-                    <TableCell sx={{ fontWeight: 'bold' }} align="middle">Price</TableCell>
-                    <TableCell sx={{ fontWeight: 'bold' }} align="middle">Check in</TableCell>
-                    <TableCell sx={{ fontWeight: 'bold' }} align="middle">Check out</TableCell>
-                    <TableCell sx={{ fontWeight: 'bold' }} align="center">Status</TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {rows.map((row) => (
-                    <TableRow
-                    key={row.name}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                        <TableCell component="th" scope="row">
-                            {row.name}
-                        </TableCell>
-                        <TableCell align="center">{row.roomnumber}</TableCell>
-                        <TableCell align="middle">{row.price}</TableCell>
-                        <TableCell align="middle">{row.checkin}</TableCell>
-                        <TableCell align="center">{row.checkout}</TableCell>
-                        <TableCell className='Status' sx={{ fontWeight: 'bold', bgcolor: 'success.main', color: 'primary.contrastText' }} align="right">{row.status}</TableCell>
-                    </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell sx={{ fontWeight: 'bold' }}>Hotel Name</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }} align="center">Room Number</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }} align="middle">Price</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }} align="middle">Check in</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }} align="middle">Check out</TableCell>
+              <TableCell sx={{ fontWeight: 'bold' }} align="center">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row._id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.hotelName}
+                </TableCell>
+                <TableCell align="center">{row.roomNumber}</TableCell>
+                <TableCell align="middle">{row.price}</TableCell>
+                <TableCell align="middle">{row.startDate}</TableCell>
+                <TableCell align="center">{row.endDate}</TableCell>
+                <TableCell className='Status' sx={{ fontWeight: 'bold', bgcolor: 'success.main', color: 'primary.contrastText' }} align="right">Success</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
-}
+};
+
+export default BasicTable;
